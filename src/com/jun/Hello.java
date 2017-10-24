5175b50c2018aaede20d7f95905802416f17a34e
kevienjun <h1253516666@gmail.com>
2017-10-24 15:43:34 +0800

    Scanner

--- src/com/jun/Hello.java
+++ src/com/jun/Hello.java
@@ -1,13 +1,23 @@
 package com.jun;
 
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    
-     Person p =  new Person();
-
-     p.sayHello();
+  
+     System.out.println("請輸入你的名字:");
+     Scanner scanner = new Scanner(System.in);
+     String line = scanner.nextLine();
+     System.out.println("Hello, " +line);
+     
+     System.out.println("你幾歲R?");
+     line = scanner.nextLine();
+     int age = Integer.parseInt(line);
+     System.out.println(age);
+     System.out.println(age > 25);
+     
 	
 	}
 
--- src/com/jun/VariableTester.java
+++ src/com/jun/VariableTester.java
@@ -16,14 +16,21 @@ public class VariableTester {
         boolean pass = false;
         System.out.println(!pass);
         System.out.println(pass);
-        String s = new String ("Hello");
+        
         Random r = new Random();
         int n1 = r.nextInt(6)+1;
         int n2 = r.nextInt(6)+1;
         int n3 = r.nextInt(6)+1;
         int n4 = r.nextInt(6)+1;
         System.out.println(3 > 2);
+        
+        String s = new String ("Hello");
+        int len = s.length();
+        System.out.println("len:" + len);
+        System.out.print(s.charAt(1));
 	
        }
 
 }
+
+
