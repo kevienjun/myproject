973c2f9ff630571ddbd62e66a92f9b3a1fdfce55
kevienjun <h1253516666@gmail.com>
2017-12-05 16:34:45 +0800

    package

---
+++ src/com/jun/Scores.java
@@ -0,0 +1,28 @@
+package com.jun;
+
+public class Scores {
+
+	public static void main(String[] args) {
+		int scores[][] = new int [5][3];
+		scores[0][0] = 55;
+		scores[1][0] = 55;
+		scores[1][1] = 55;
+		scores[1][2] = 55;
+		scores[2][0] = 55;
+		scores[2][1] = 55;
+		scores[2][2] = 55;
+		scores[3][0] = 55;
+		scores[3][1] = 55;
+		scores[3][2] = 55;
+		scores[4][0] = 55;
+		scores[4][1] = 55;
+		scores[4][2] = 55;
+		
+		for (int i=0; i<5; i++){
+			System.out.print(scores[i][0]+" "+scores)
+		}
+		
+
+	}
+
+}
---
+++ src/com/jun/Tester.java
@@ -0,0 +1,16 @@
+package com.jun;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Student stu = new Student("ÁÂ®aºÓ",55,66,77);
+		/*/stu.english = 55;
+		stu.math = 66;
+		stu.chinese = 77;/*/
+		stu.print();
+		int avg = stu.getAverage();
+		System.out.println("ÁÂ®aºÓ"+avg);
+
+	}
+
+}
