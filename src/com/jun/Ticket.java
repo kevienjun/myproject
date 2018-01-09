35b5975d57804d9b3cb35b990b0da0d4403538a3
kevienjun <h1253516666@gmail.com>
2018-01-09 16:27:31 +0800

    ticket

---
+++ src/com/jun/Ticket.java
@@ -0,0 +1,10 @@
+package com.jun;
+
+public class Ticket {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- src/com/jun/sogo/Tester.java
+++ src/com/jun/sogo/Tester.java
@@ -1,16 +1,36 @@
 package com.jun.sogo;
 
+import java.util.ArrayList;
+
 public class Tester {
 
 	public static void main(String[] args) {
-		Customer c1 = new Customer(1000);
-		SilverCustomer c2 = new SilverCustomer(1000);
-		GoldenCustomer c3 = new GoldenCustomer(1000);
-		c1.print();
-		c2.print();
-		c3.print();
-		
-		
+		ArrayList<Customer> customers = new ArrayList<>();
+		customers.add(new Customer(10000));
+		customers.add(new SilverCustomer(10000));
+		customers.add(new GoldenCustomer(10000));
+		for (int i = 0; i < customers.size(); i++) {
+			Customer c = customers.get(i);
+			c.print();
+		}
+		ArrayList<Integer> list = new ArrayList<>();
+		list.add(94);
+		list.add(87);
+		list.add(55);
+		list.add(4);
+		int n = list.get(2);
+		int n2 = list.get(3);
+		System.out.println(list.get(1));
+		System.out.println(list.size());
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+
+		/*
+		 * / Customer c1 = new Customer(1000); SilverCustomer c2 = new
+		 * SilverCustomer(1000); GoldenCustomer c3 = new GoldenCustomer(1000);
+		 * c1.print(); c2.print(); c3.print();/
+		 */
 
 	}
 
