f408d774193dc7c042e2c1c26632605d6ec972bf
kevienjun <h1253516666@gmail.com>
2018-03-08 16:02:02 +0800

    private public作業(未完成)

---
+++ src/com/java2/object/Account.java
@@ -0,0 +1,10 @@
+package com.java2.object;
+
+public class Account {
+	public static void main(String args[]){
+
+		Password fone = new Password("æl√B:15,000"); 
+		System.out.println(fone.getaccount()); 
+		}
+
+}
---
+++ src/com/java2/object/Password.java
@@ -0,0 +1,23 @@
+package com.java2.object;
+
+public class Password {
+
+	
+	private String telno = "æl√B:20,000";
+
+	Password(String str) {
+	
+
+		telno = str;
+
+	}
+	
+
+	public String getaccount() {
+
+		return telno;
+
+	}
+
+}
+
